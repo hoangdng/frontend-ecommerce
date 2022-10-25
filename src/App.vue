c92103df554c0b41a7e32ab4434063acc5610def
hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
2022-10-25 16:21:06 +0700

    Init project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-primary text-white" elevated height-hint="98">
+      <div class="flex justify-between q-px-lg q-py-sm">
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+        </q-avatar>
+        <q-tabs align="left">
+          <q-route-tab label="Page One" to="/page1" />
+          <q-route-tab label="Page Two" to="/page2" />
+          <q-route-tab label="Page Three" to="/page3" />
+        </q-tabs>
+      </div>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white" elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout"
+};
+</script>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+import { Quasar } from 'quasar'
+import quasarUserOptions from "./quasar-user-options";
+import "quasar/dist/quasar.sass"
+
+const app = createApp(App).use(Quasar, quasarUserOptions)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
